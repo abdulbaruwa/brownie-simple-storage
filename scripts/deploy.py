c47d4c323f180ee22f5758911451c42398bbb5e2
Shangoman <abdulbaruwa@hotmail.com>
2021-09-30 08:05:33 +0100

    Call contract view and transaction endpoints

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,24 @@
-from brownie import accounts
-import os
+from brownie import accounts, config, SimpleStorage
 
 def deploy_simple_storage():
-     account = accounts.add(os.getenv("PRIVATE_KEY"))
-     print(account)
+     # account = accounts.add(config["wallets"]["from_key"])
+     account = accounts[0]
+     simple_storage = SimpleStorage.deploy({"from": account})
+     print(simple_storage)
+
+     # a simple retrieve from the contract, calling the retrieve function on contract
+     stored_value = simple_storage.retrieve()
+     print(stored_value)
+
+     # calling a transaction function
+     # note: In browniew, we need to pass the from account with the call
+     transaction = simple_storage.store(15, {"from": account})
+     transaction.wait(1)
+
+     # retrieve the latest state from the chain
+     updated_stored_value = simple_storage.retrieve()
+     print(updated_stored_value)
+
 
 
 def main():
